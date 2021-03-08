6f12e585f3c0cf0a5a4648828aeec7a1c59c9abb
guoshoujing <gsj1075185785@gmail.com>
2021-03-08 10:50:58 +0800

    add fork test file

---
+++ src/forkTest/ForkTest.java
@@ -0,0 +1,11 @@
+package forkTest;
+
+/**
+ * @author guoshoujing
+ * @create 2021-03-08 上午10:49
+ */
+public class ForkTest {
+    public static void main(String[] args) {
+        System.out.println("I'm a fork test file");
+    }
+}
